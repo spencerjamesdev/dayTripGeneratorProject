e4e25d557259c86aa956302b75eb3e081840d800
spencerjamesdev <spencer.franke@gmail.com>
2021-07-05 15:22:17 -0500

    Latest Version

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -49,7 +49,7 @@ function randomEntertainment(array){
 //This function will allow the user to re-select 
 //Dest, Restr,transp, entertain
 function randomReselect(string){
-    if(string === "Destination"){
+    if(string === "1"){
         let newDestination = getRandom(destinationArray);
         while(currentDestination == newDestination){
             newDestination = getRandom(destinationArray);
@@ -58,7 +58,7 @@ function randomReselect(string){
             currentDestination = newDestination;
         } 
     }
-    else if(string === "Restaurant"){
+    else if(string === "2"){
         let newRestaurant = getRandom(restaurantArray);
         while(currentRestaurant == newRestaurant){
             newRestaurant = getRandom(restaurantArray);
@@ -67,7 +67,7 @@ function randomReselect(string){
             currentRestaurant = newRestaurant;
         }
     }
-    else if(string === "Transportation"){
+    else if(string === "3"){
         let newTransportation = getRandom(transportationArray);
         while(currentTransportation == newTransportation){
             newTransportation = getRandom(transportationArray);
@@ -76,7 +76,7 @@ function randomReselect(string){
             currentTransportation = newTransportation;
         }
     }
-    else if(string === "Entertainment"){
+    else if(string === "4"){
         let newEntertainment = getRandom(entertainmentArray);
         while(currentEntertainment == newEntertainment){
             newEntertainment = getRandom(entertainmentArray);
@@ -97,18 +97,19 @@ let currentTransportation = getRandom(transportationArray);
 let currentEntertainment = getRandom(entertainmentArray);
 let userChoice = "No";
 
-while (userChoice === "No"){
+while (userChoice == "No" || userChoice == "no"){
     
     userChoice = prompt(`Destination:   ${currentDestination} \nRestaurant:   ${currentRestaurant} \nTransportation:   ${currentTransportation} \nEntertainment:   ${currentEntertainment} \nAre you happy with these options? \nPlease enter Yes or No.`);
     
-    if(userChoice === "No"){
-        randomReselect(prompt("Please enter one of the following you would like to change; Destination, Restaurant, Transportation, or Entertainment?"));
+    if(userChoice == "No" || userChoice == "no"){
+        randomReselect(prompt("Which of the following you would like to change; \nEnter '1' for Destination, \n'2' for Restaurant, \n'3' for Transportation, \n'4' for Entertainment?"));
     }
-    else if(userChoice === "Yes"){
+    else if(userChoice == "Yes" || userChoice == "yes"){
         console.log(`Your day trip is complete! Your destination will be ${currentDestination}. You will be eating at ${currentRestaurant}. Your mode of transportation will be ${currentTransportation}. And your form of entertainment will be ${currentEntertainment}. I hope you enjoy your trip!`);
         break;
     }
 }
+
 
 
 
